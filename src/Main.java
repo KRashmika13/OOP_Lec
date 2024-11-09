b768854a3a4067c51b5ac44fa5d991d711d9bb78
KRashmika13 <krashmika13@gmail.com>
2024-11-09 16:41:00 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        StrringLength s1 = new StrringLength();
+
+
+    }
+}
---
+++ src/StrringLength.java
@@ -0,0 +1,41 @@
+public class StrringLength {
+
+   public static void main(String[] args) {
+       String sample = "    Hello world";
+
+       System.out.println("the length of string: " + sample.length());
+
+       System.out.println("is string equal to: " + sample.trim().equals(sample));
+
+       char older = 'e';
+       char newer = '0';
+
+       String newSample = sample.replace(older, newer);
+       System.out.println("replaced string: " + newSample);
+
+       String oldSubstring = "Hello";
+       String newSubstring = "Hii";
+
+       String newSample2 = oldSubstring + newSubstring;
+       System.out.println("replaced string: " + newSample2);
+
+       String s1 = "kavi";
+       String s2 = "kavi";
+
+       System.out.println(s1 == s2);
+       System.out.println(s1.equals(s2));
+
+       String s3 = new String("kavi");
+       String s4 = new String("kavi");
+       System.out.println(s1 == s3);
+       System.out.println(s1.equals(s3));
+
+       String interned1 = s3.intern();
+       String intern2 = s4.intern();
+       System.out.println(interned1 == intern2);
+
+       
+
+
+   }
+}
